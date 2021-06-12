a0e4f6820922e2814e1da0fca45f3ac464f55061
bmk15897 <bmk15897@gmail.com>
2021-06-12 20:19:17 +0530

    india map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import highmaps from 'highcharts/modules/map.src';
+import more from 'highcharts/highcharts-more.src';
 import { AppComponent } from './app.component';
 
+export function highchartsModules() {
+  // apply Highcharts Modules to this array
+  return [more, highmaps];
+}
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, ChartModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: HIGHCHARTS_MODULES, useFactory: highchartsModules } // add as factory to your providers
+  ]
 })
 export class AppModule { }
